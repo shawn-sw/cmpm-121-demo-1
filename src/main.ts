c49c0639504447b3398df6dd3eb42c3d27416949
shawn-sw <146413305+shawn-sw@users.noreply.github.com>
2024-10-13 23:11:45 -0400

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,46 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Mushroom Forest";
-
 document.title = gameName;
 
+// 创建标题
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// adds counter
+// 创建游戏计数器容器
 const gameDiv: HTMLDivElement = document.createElement("div");
 gameDiv.id = "gameCounter";
 app.append(gameDiv);
 
+// 创建计数显示区域
 const count: HTMLDivElement = document.createElement("div");
 count.id = "count";
 count.innerHTML = "Count: 0<br><br>";
 count.style.textAlign = "center";
 gameDiv.append(count);
+
+// mushroom count
 let mushroom = 0;
 
-// adds button
+// add button
 const bookButton: HTMLButtonElement = document.createElement("button");
 bookButton.innerHTML = "Collect 🍄";
 bookButton.id = "bookButton";
 gameDiv.append(bookButton);
 
-// add event listeners to the button
+// add event listener
 bookButton.addEventListener("click", () => {
-  mushroom++;
-  count.innerHTML = `Count: ${mushroom}<br><br>`;
+  incrementMushroom(1); // click +1
 });
+
+// add function to update mushroom number
+function incrementMushroom(val: number = 1) {
+  mushroom += val;
+  count.innerHTML = `Count: ${mushroom.toFixed(2)}<br><br>`;
+}
+
+// increase by time
+setInterval(() => {
+  incrementMushroom(1 / 60);
+}, 1000 / 60);
