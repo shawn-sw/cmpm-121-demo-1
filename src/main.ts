bd9de517cc1b01c1be47c46f2ba02c0f1adf50b7
shawn-sw <146413305+shawn-sw@users.noreply.github.com>
2024-10-13 22:49:07 -0400

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,26 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// adds counter
+const gameDiv: HTMLDivElement = document.createElement("div");
+gameDiv.id = "gameCounter";
+app.append(gameDiv);
+
+const count: HTMLDivElement = document.createElement("div");
+count.id = "count";
+count.innerHTML = "Count: 0<br><br>";
+count.style.textAlign = "center";
+gameDiv.append(count);
+let mushroom = 0;
+
 // adds button
 const bookButton: HTMLButtonElement = document.createElement("button");
 bookButton.innerHTML = "Collect 🍄";
 bookButton.id = "bookButton";
+gameDiv.append(bookButton);
+
+// add event listeners to the button
+bookButton.addEventListener("click", () => {
+  mushroom++;
+  count.innerHTML = `Count: ${mushroom}<br><br>`;
+});
